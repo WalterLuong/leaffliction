d5dc5ad025260973fc372ebcfd6fcb783bf28e9f
ThePush <jeremysemel@gmail.com>
2023-07-10 21:57:51 +0200

    feat(vgg_from_scratch.py): working on mc computer

--- vgg_from_scratch.py
+++ vgg_from_scratch.py
@@ -1,25 +1,29 @@
 import matplotlib.pyplot as plt
-from keras.callbacks import ModelCheckpoint, EarlyStopping
-from keras.optimizers import Adam
-import keras
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras.optimizers import Adam
 import os
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPool2D, Flatten
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 from pathlib import Path
+import tensorflow as tf
 
-dataset_train_path = Path('data', 'images', 'Grape')
-dataset_valid_path = Path('data', 'valid', 'Grape')
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+dataset_train_path = Path('data', 'images', 'Apple')
+dataset_valid_path = Path('data', 'valid', 'Apple')
 
 
 traindata = ImageDataGenerator().flow_from_directory(
-    directory=dataset_train_path, target_size=(224, 224))
+    directory=dataset_train_path, target_size=(200, 200))
 testdata = ImageDataGenerator().flow_from_directory(
-    directory=dataset_valid_path, target_size=(224, 224))
+    directory=dataset_valid_path, target_size=(200, 200))
 
 model = Sequential()
-model.add(Conv2D(input_shape=(224, 224, 3), filters=64,
+model.add(Conv2D(input_shape=(200, 200, 3), filters=64,
           kernel_size=(3, 3), padding="same", activation="relu"))
 model.add(Conv2D(filters=64, kernel_size=(3, 3),
           padding="same", activation="relu"))
@@ -58,7 +62,7 @@ model.add(Dense(units=4, activation="softmax"))
 
 opt = Adam(learning_rate=1e-4)
 model.compile(optimizer=opt,
-              loss=keras.losses.categorical_crossentropy, metrics=['accuracy'])
+              loss=tf.keras.losses.categorical_crossentropy, metrics=['accuracy'])
 
 model.summary()
 
@@ -68,16 +72,14 @@ early_stopping = EarlyStopping(monitor='accuracy',
                                patience=20, verbose=1, mode='max')
 callbacks = [checkpoint, early_stopping]
 
-hist = model.fit(traindata, batch_size=1, steps_per_epoch=10, epochs=100,
+hist = model.fit(traindata, batch_size=2, steps_per_epoch=10, epochs=100,
                  verbose=1, callbacks=callbacks)
 
-plt.plot(hist.history["acc"])
 plt.plot(hist.history['accuracy'])
 plt.plot(hist.history['loss'])
-plt.plot(hist.history['val_loss'])
 plt.title("model accuracy")
 plt.ylabel("Accuracy")
 plt.xlabel("Epoch")
-plt.legend(["Accuracy", "Validation Accuracy", "loss", "Validation Loss"])
+# plt.legend(["Accuracy", "Validation Accuracy", "loss", "Validation Loss"])
 plt.savefig('vgg16_1.png')
 plt.show()
