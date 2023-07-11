aed050beb6558601c7f2455f6d0a5719480f984c
Jeremy <jeremysemel@gmail.com>
2023-07-11 10:27:45 +0200

    clean(train.py/vgg_from_scratch.py)

--- train.py
+++ train.py
@@ -1,15 +1,27 @@
 from fastai.vision.all import ImageDataLoaders, accuracy, vision_learner, models
 from pathlib import Path
 
+
+# Plant selection and dataset paths
 plant = 'Grape'
 dataset_train_path = Path('data/images', plant)
 
+
+# Load dataset
 data = ImageDataLoaders.from_folder(
     dataset_train_path, valid_pct=0.2, size=224, num_workers=4, bs=4)
 
+
+# Model definition (VGG16)
 learn = vision_learner(data, models.vgg16_bn, metrics=accuracy)
 learn.model
+
+
+# Train model
 learn.fit(2)
 learn.save('stage-1')
 learn.export('export.pkl')
+
+
+# Evaluate model
 learn.show_results()
--- vgg_from_scratch.py
+++ vgg_from_scratch.py
@@ -10,20 +10,25 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 
 
+# GPU memory management
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 if len(physical_devices) > 0:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
+# Plant selection and dataset paths
 plant = 'Apple'
 dataset_train_path = Path('data', 'images', plant)
 dataset_valid_path = Path('data', 'valid', plant)
 
 
+# Load dataset
 traindata = ImageDataGenerator().flow_from_directory(
     directory=dataset_train_path, target_size=(200, 200))
 testdata = ImageDataGenerator().flow_from_directory(
     directory=dataset_valid_path, target_size=(200, 200))
 
+
+# Model definition (VGG16)
 input_shape = (200, 200, 3)
 inputs = Input(shape=input_shape)
 
@@ -66,13 +71,14 @@ outputs = Dense(units=4, activation="softmax")(x)
 
 model = Model(inputs=inputs, outputs=outputs)
 
-
 opt = Adam(learning_rate=1e-4)
 model.compile(optimizer=opt,
               loss=SparseCategoricalCrossentropy, metrics=['accuracy'])
 
 model.summary()
 
+
+# Train
 checkpoint = ModelCheckpoint(f'{plant}_vgg16.h5', monitor='val_accuracy', verbose=1,
                              save_best_only=True, mode='max', save_freq=1)
 early_stopping = EarlyStopping(monitor='val_accuracy',
@@ -82,6 +88,8 @@ callbacks = [checkpoint, early_stopping]
 hist = model.fit(traindata, batch_size=2, steps_per_epoch=10, epochs=100,
                  verbose=1, callbacks=callbacks)
 
+
+# Plot
 plt.plot(hist.history['accuracy'])
 plt.plot(hist.history['val_accuracy'])
 plt.title('Model Accuracy')
