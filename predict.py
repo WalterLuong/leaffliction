6d711b3c96aeff0b25978c8beec4f290e4f5cb5d
Jeremy <jeremysemel@gmail.com>
2023-07-17 14:53:49 +0200

    feat(predict.py): can take a path to zipped model as argument

--- predict.py
+++ predict.py
@@ -21,9 +21,6 @@ logger = logging.getLogger(__name__)
 warnings.filterwarnings("ignore")
 
 
-SOTA_MODEL = Path(
-    'data/zipped_files/vgg19_SOTA/vgg19_SOTA.pkl')
-
 y_true = []
 y_pred = []
 
@@ -34,6 +31,7 @@ def unzip_model(
 ) -> Path:
     if not model_path.exists():
         raise FileNotFoundError('Model does not exist.')
+
     try:
         logger.info(f'Unzipping {model_path.name}')
         extract_dir = Path(model_path.parent, model_path.stem)
@@ -42,7 +40,7 @@ def unzip_model(
         with ZipFile(model_path, 'r') as zipObj:
             zipObj.extractall(extract_dir)
         logger.info('Success')
-        return (Path(extract_dir, 'vgg19_SOTA.pkl'))
+        return (Path(extract_dir, Path(model_path.stem).with_suffix('.pkl')))
     except Exception as e:
         logger.error('Error unzipping model.')
         raise e
@@ -79,7 +77,7 @@ def plot_confusion_matrix(y_true: list, y_pred: list) -> None:
 
 
 def predict_image(
-    model_path: Path = Path('data/zipped_files/vgg19_SOTA.pkl'),
+    model_path: Path = Path('data/zipped_files/vgg19_SOTA/vgg19_SOTA.pkl'),
     image_path: Path = Path('data/valid/')
 ) -> None:
     model = torch.load(model_path)
@@ -94,7 +92,7 @@ def predict_image(
             pred_class, pred_idx, outputs = model.predict(image)
             y_true.append(true_class)
             y_pred.append(pred_class)
-            tqdm.write(f"Progress: {index}/{len(images)}")
+            tqdm.write(f"Progress: {index+1}/{len(images)}")
             print_prediction(image, pred_class, pred_idx, outputs, true_class)
         print(classification_report(y_true, y_pred))
         plot_confusion_matrix(y_true, y_pred)
@@ -128,13 +126,28 @@ if __name__ == '__main__':
         default=Path('data/valid/'),
         help='Path to image/dataset to test the model on'
     )
+    parser.add_argument(
+        '--model',
+        type=str,
+        default=Path('data/zipped_files/vgg19_SOTA.zip'),
+        help='Path to model to use'
+    )
     args = parser.parse_args()
 
     try:
         if not Path(args.path).exists():
             raise FileNotFoundError('Path does not exist.')
 
-        model_path = unzip_model() if not SOTA_MODEL.exists() else SOTA_MODEL
+        unzipped_model_path = Path(
+            'data',
+            'zipped_files',
+            Path(args.model).stem,
+            Path(args.model).stem+'.pkl'
+        )
+        model_path = unzip_model(
+            Path(args.model)) \
+            if not unzipped_model_path.exists() \
+            else unzipped_model_path
         predict_image(model_path, Path(args.path))
     except Exception as e:
         logger.error(e)
